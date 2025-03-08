c2716a2f06acca977f57b16f22befd0b542b152b
khanh-hoho <khanhtd@ti-one.de>
2025-03-08 22:30:14 +0700

    update file start

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-mkdir -p /mnt/ssd/uploads_service/statics/uploads
-cd /mnt/ssd/uploads_service/upload_file
+mkdir -p /mnt/ssd/upload_file/asset
+cd /mnt/ssd/upload_file
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
